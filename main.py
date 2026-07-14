92aaa8cc179d750770d5fed23254145bf345658f
Hellz35u <hellz35u@gmail.com>
2026-07-15 02:06:54 +0300

    Implement user authentication and database integration for scan results
    - Added user authentication options (login and registration) in `main.py`, allowing users to securely access the scanning features.
    - Updated `save_scan_results` function in `reports.py` to store scan results in a SQLite database instead of a JSON file.
    - Enhanced user prompts and input validation for improved clarity and user experience.
    - Integrated functionality to save scan results associated with the authenticated user.

--- main.py
+++ main.py
@@ -1,61 +1,118 @@
+from database import initialize_database, login, register
 from reports import save_scan_results
 from scanner import ports_scanner
 from scan_modes import get_full_scan, get_quick_scan, get_custom_scan
 from validators import validate_ip, validate_port, validate_port_range
 from services import get_service_name
 
-choice = int(input("Chose scan mode:\n [1] - Quick Scan(1 - 1024)\n [2] - Full Scan(1 - 65535)\n [3] - Custom Scan\n [4] - EXIT\n Enter your choice number: "))
+initialize_database()
+
+print("[1] - Login")
+print("[2] - Register")
+
+authentication_choice = input("Enter your choice: ")
+
+
+if authentication_choice == "1":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    current_user = login(username, password)
+    
+    if current_user is None:
+        exit()
+    
+elif authentication_choice == "2":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if register(username, password):
+        print("Registration completed successfully")
+
+        current_user = login(username, password)
+
+        if current_user is None:
+            exit()
+    else:
+        print("Username already exists.")
+        exit()
+
+
+print(
+    "Choose scan mode:\n"
+    "[1] - Quick Scan (1 - 1024)\n"
+    "[2] - Full Scan (1 - 65535)\n"
+    "[3] - Custom Scan\n"
+    "[4] - Exit"
+)
+
+choice = input("Enter your choice: ")
 
 match choice:
-    case 1:
+    case "1":
         ports = get_quick_scan()
-    
-    case 2:
+
+    case "2":
         ports = get_full_scan()
 
-    case 3:
-        start_port = input("Enter Starting port:")
-        end_port = input("Enter Ending port:")
-        ports = get_custom_scan(start_port, end_port)
-        
+    case "3":
+        start_port = input("Enter starting port: ")
+        end_port = input("Enter ending port: ")
+
         if not validate_port(start_port):
-            print("start port must be a number between 1 - 65535")
+            print("Starting port must be a number between 1 and 65535.")
             exit()
-        
+
         if not validate_port(end_port):
-            print("end port must be a number between 1 - 65535")
+            print("Ending port must be a number between 1 and 65535.")
             exit()
 
-        if not not validate_port_range(start_port, end_port):
-            print("Starting port must be smaller than or equal to Ending port")
+        if not validate_port_range(start_port, end_port):
+            print("Starting port must be smaller than or equal to ending port.")
             exit()
-    
-    case 4:
-        print("Good bye:)")
-    
+
+        ports = get_custom_scan(start_port, end_port)
+
+    case "4":
+        print("Goodbye :)")
+        exit()
+
     case _:
-        print("Invalid Choice")
-    
+        print("Invalid choice.")
+        exit()
 
-ip = input("Enter IP:")
+
+ip = input("Enter IP: ")
 
 if not validate_ip(ip):
-    print("Invalid IP Address")
+    print("Invalid IP address.")
     exit()
 
-print(f"Scanning IP: {ip}. . .")
+
+print(f"Scanning IP: {ip}...")
+
 
 open_ports = []
 open_port_numbers = ports_scanner(ip, ports)
 
+
 for port in open_port_numbers:
     service = get_service_name(port)
+
     open_ports.append({
-        "port":port,
+        "port": port,
         "service": service
     })
 
-    print(f"Port {port} is OPEN on Service {service}")
+    print(f"Port {port} is OPEN — Service: {service}")
 
-save_scan_results(ip, open_ports)
+
+if save_scan_results(
+    current_user["id"],
+    ip,
+    open_ports
+):
+    print("Scan results saved successfully.")
+else:
+    print("Failed to save scan results.")
 
--- reports.py
+++ reports.py
@@ -1,27 +1,78 @@
-import os
+import sqlite3
 import json
 from datetime import datetime
 
-def save_scan_results(ip, open_ports):
-    scan_results = {
-        "ip" : ip,
-        "scan_time" : datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "open_ports" : open_ports
-    }
-
-    os.makedirs("results",exist_ok=True)
-    
-    file_name = datetime.now().strftime("scan_%Y-%m-%d_%H-%M-%S.json")
-    file_path = os.path.join("results", file_name)
+def save_scan_results(user_id, ip, open_ports):
+    conn = None
 
     try:
-        with open(file_path,"w") as file:
-            json.dump(scan_results, file, indent=4)
+        conn = sqlite3.connect("port_scanner.db")
+        conn.execute("PRAGMA foreign_keys = ON")
+        cursor = conn.cursor()
+        
+        open_ports_json = json.dumps(open_ports)
+        created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    except OSError as e:
-        print(f"Failed save scan results: {e}")
+        conn.execute(
+            """INSERT INTO reports(
+                user_id,
+                target_ip, 
+                open_ports, 
+                created_at
+            )
+            VALUES(?, ?, ?, ?)""",
+            (
+                user_id,
+                ip,
+                open_ports_json,
+                created_at
+            )
+        )
+        conn.commit()
+        return True
+    
+    except sqlite3.Error as e:
+        print(f"Failed to save scan results: {e}")
         return False
     
-    return True
+    finally:
+        if conn is not None:
+            conn.close()
     
+def get_reports_by_user_id(user_id):
+    conn = sqlite3.connect("port_scanner.db")
+    cursor = conn.cursor()
+
+    conn.execute(
+        """
+        SELECT id, target_ip, open_ports, created_at
+        FROM reports
+        WHERE user_id = ?
+        ORDER BY created_at DESC
+        """,
+        (user_id)
+    )
+    reports = cursor.fetchall()
+    conn.close()
+
+    return reports
+
+def get_report_by_id(id):
+    conn = sqlite3.connect("port_scanner.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT id, target_ip, open_ports, created_at
+        FROM reports
+        WHERE id = ?
+        """,
+        (id)
+    )
+    report = cursor.fetchone()
+    conn.close()
+
+    return report
+
+
 
